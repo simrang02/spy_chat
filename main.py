c22a057d56e9f981c3834c40f37f5d01955f88b1
simrang02 <simrangoyal278@gmail.com>
2018-03-21 11:42:38 +0530

    greeting message

---
+++ main.py
@@ -0,0 +1,8 @@
+print("THE SPY CHAT!")
+#asking name
+spy_name = input("What's your name?")
+#asking for salutation
+spy_salutation = input("What would you like us to call you (Mr., Ms. or Mrs.) ?")
+#welcome
+print("Welcome to the spy chat " + spy_salutation + " " + spy_name)
+print("Alright " + spy_salutation + " " + spy_name + " I'd like to know a little bit more about you...")
